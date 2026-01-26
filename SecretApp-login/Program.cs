c6511ef904fea2efa1c6ea2faed94edb30586756
william zander <willman.zander@gmail.com>
2026-01-26 14:27:09 +0100

    Add project files.

---
+++ SecretApp-login/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SecretApp_login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("välkommen");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine(
+                "\n\n***********************\n" +
+                "1. logga in\n" +
+                "2. lägg till användare\n" + 
+                "3. ändra lösenord\n" +
+                "4. visa användar lista\n" +
+                "9. visa menyn\n" +
+                "0. avsluta\n"
+                );
+
+        }
+    }
+}
